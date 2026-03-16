0eee48fffb8e456cf32fd4d1cd0f410205328a74
himanshusainiyt2524-max <himanshusainiyt2524@gmail.com>
2026-03-16 19:19:38 +0530

    Add files via upload

---
+++ Assignment 2 ( Task 1).py
@@ -0,0 +1,4 @@
+""" Checking whether the number is even or odd using an if-else
+"""
+Number = int(input("Enter a number: "))
+print( "is even number") if Number%2==0 else print("odd ")
---
+++ Assignment 2 ( Task 2 ).py
@@ -0,0 +1,7 @@
+#Sum of Integers from 1 to 50 Using a Loop
+
+GOD=0
+for i in range(1, 51):
+    GOD = GOD + i
+
+print(" The Sum of number from 1 to 50 is :", GOD)
